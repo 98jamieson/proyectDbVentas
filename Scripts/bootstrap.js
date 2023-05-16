696d6ed4c2c466d97120523a59828b419f980bbc
gladysespynn <gchenc1@miumg.edu.gt>
2023-05-16 16:55:39 -0600

    feature/gc-001 --Este es mi primer cambio en GIT

--- Scripts/bootstrap.js
+++ Scripts/bootstrap.js
@@ -29,7 +29,7 @@ if (typeof jQuery === 'undefined') {
  * Copyright 2011-2019 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
-
+    /* wenasssssssssssssssssss*/ 
 
 +function ($) {
   'use strict';
